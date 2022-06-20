4d9b669f94c86e8a6ba492ee388242da66f132e6
Sai Sahith V K <vksaisahith272@gmail.com>
2022-06-20 12:01:02 +0530

    Trans layer model

--- models/bi_layer/order_details_bi.sql
+++ models/bi_layer/order_details_bi.sql
@@ -6,4 +6,22 @@
 
 }}
 
-select * from {{ ref('order_details_trans') }}
+select 
+    O_ORDERKEY,
+    P_PARTKEY,
+    C_CUSTKEY,
+    C_NAME,
+    P_NAME,
+    P_BRAND,
+    O_ORDERDATE,
+    O_ORDERPRIORITY,
+    QTY,
+    PRICE,
+    L_EXTENDEDPRICE,
+    CALC_PRICE,
+    PRICE_CHECK_FLAG,
+    L_LINESTATUS,
+    L_SHIPDATE,
+    L_SHIPMODE
+ from {{ ref('order_details_trans') }}
+ 
