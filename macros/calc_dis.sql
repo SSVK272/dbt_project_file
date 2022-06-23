7e80ccaa0913afcbeeb4d3ab4e8ea71bdd488421
Sai Sahith V K <vksaisahith272@gmail.com>
2022-06-23 12:38:43 +0530

    macros

--- macros/calc_dis.sql
+++
@@ -1,9 +0,0 @@
-{% macro calc_dis(mkseg,p) %}
-    {% set seg=['AUTOMOBILE','MACHINERY'] %} 
-    {% if mkseg in seg %}
-        {{ return(p*0.12) }}
-    {% else %}
-        {{ return(p*0.04) }}
-    {% endif %}
-    
-{% endmacro %}
